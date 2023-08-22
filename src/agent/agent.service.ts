35b1d46db3b2fee8100713ae377fa3aeda7e1e59
Utpal Singh <utpalsingh915@gmail.com>
2023-08-22 10:55:02 +0530

    add agent and get agent

--- src/agent/agent.service.ts
+++ src/agent/agent.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { CreateAgentDto } from './dto/create-agent.dto';
 import { UpdateAgentDto } from './dto/update-agent.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { ROLE } from '@prisma/client';
+import { hash } from 'bcrypt';
 
 @Injectable()
 export class AgentService {
@@ -15,6 +17,7 @@ export class AgentService {
       }
     });
   }
+  
   async findAllEmployee() {
     return await this.prisma.employee.findMany({
       where: {
@@ -27,8 +30,46 @@ export class AgentService {
     });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} agent`;
+  async createEmployee(body: CreateAgentDto) {
+    const applicationId = await this.prisma.employee.findMany({
+      take: 1,
+      where: {
+        park: false,
+      },
+      orderBy: {
+        id: 'desc'
+      }
+    })
+
+    const last_application = applicationId[0]
+    let a_id = "EMID"
+    if (!last_application?.employeeCode) {
+      a_id = a_id + "0001"
+    } else {
+      const last_id = last_application.employeeCode
+      const _id = last_id.split("EMID")[1]
+      const id = parseInt(_id) + 1
+      a_id = a_id + id.toString().padStart(4, '0')
+    }
+
+    const create_agent = await this.prisma.employee.create({
+      data: {
+        title: body.title,
+        firstName: body.firstName,
+        LastName: body.LastName,
+        role: body.role as ROLE,
+        email: body.Email,
+        status: "APPROVED",
+        joinedAt: new Date().toISOString().slice(0, -1) + "Z",
+        designation: body.designation,
+        phone: body.Phone,
+        city: body.city,
+        employeeCode: a_id,
+        password: await hash(body.password, 10),
+        managedById: Number(body.workUnder) || null
+      }
+    })
+    return create_agent;
   }
 
   update(id: number, updateAgentDto: UpdateAgentDto) {
--- src/agent/dto/create-agent.dto.ts
+++ src/agent/dto/create-agent.dto.ts
@@ -1,4 +1,45 @@
+import { ROLE } from '@prisma/client';
+import { IsIn, IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
 export class CreateAgentDto {
-    applicationId: string;
+    @IsNotEmpty()
+    @IsString()
+    title: string
+
+    @IsNotEmpty()
+    @IsString()
+    firstName: string
+
+    @IsNotEmpty()
+    @IsString()
+    LastName: string
+
+    @IsNotEmpty()
+    @IsString()
+    @IsIn(["AGENT", "DEALERSHIP", "FEILDOFFICER"])
+    role: ROLE
+
+    @IsNotEmpty()
+    @IsString()
+    Email: string
+
+    @IsNotEmpty()
+    @IsString()
+    Phone: string
+
+    @IsNumber()
+    @IsOptional()
+    workUnder: number
+
+    @IsNotEmpty()
+    @IsString()
+    city: string
+
+    @IsNotEmpty()
+    @IsString()
+    password: string
+
+    @IsNotEmpty()
+    @IsString()
+    designation: string
     
 }
