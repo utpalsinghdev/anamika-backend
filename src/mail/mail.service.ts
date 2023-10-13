5da5880b026ba7bc7abe92cb0e16e4fcba561580
Utpal Singh <utpalsingh915@gmail.com>
2023-10-13 20:15:36 +0530

    port updated

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -5,23 +5,7 @@ import axios from 'axios';
 @Injectable()
 export class MailService {
   async sendSms(createMailDto: CreateMailDto) {
-    const api = process.env.SMSAPIKEY
 
-    const options = {
-      method: 'POST',
-      url: 'https://www.fast2sms.com/dev/bulkV2',
-      headers: {
-        'Content-Type': 'application/json',
-        'User-Agent': 'insomnia/2023.5.8',
-        Authorization: 'mk4UMJXesxFQ8fOhgT3wWcB1LG9bZu5oPpaVijH7nz2rAvYR0DItlfV6SWMbck0CPTNi7RD18EQhJsu3'
-      },
-      data: {
-        route: 'q',
-        message: createMailDto.message,
-        flash: 0,
-        numbers: createMailDto.numbers
-      }
-    };
     try {
       // const response = await axios.request(options)
       // console.log(response.data);
