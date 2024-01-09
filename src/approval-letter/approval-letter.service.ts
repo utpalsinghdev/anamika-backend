eb7591d28ba396bbc3f03764e9209de1e9a11aa6
Utpal Singh <utpalsingh915@gmail.com>
2024-01-09 20:40:10 +0530

    Update headless mode in puppeteer launch

--- src/approval-letter/approval-letter.service.ts
+++ src/approval-letter/approval-letter.service.ts
@@ -101,7 +101,7 @@ export class ApprovalLetterService {
       const browser = await puppeteer.launch(
         {
           args: ["--no-sandbox", "--disable-setuid-sandbox"],
-          headless: false,
+          headless: true,
         }
       );
       const page = await browser.newPage();
