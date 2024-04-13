dbd7bbf802904450534c62e4715c263be45df3d9
Utpal Singh <utpalsingh915@gmail.com>
2024-04-13 11:14:08 +0530

    Remove puppeteer dependency and unused code

--- src/approval-letter/approval-letter.service.ts
+++ src/approval-letter/approval-letter.service.ts
@@ -9,7 +9,6 @@ import { exec, execSync } from 'child_process';
 import * as fs from 'fs';
 import * as path from 'path';
 
-import puppeteer from 'puppeteer';
 import * as dayjs from 'dayjs';
 @Injectable()
 export class ApprovalLetterService {
@@ -110,19 +109,6 @@ export class ApprovalLetterService {
         payLink: paymnt[0].url,
 
       }
-
-      const { originalPdfPath, encryptedPdfPath, password, pdfName } = await this.pdfLink(dynamicData)
-
-      console.log(password)
-      await this.prisma.approvalLetter.update({
-        where: {
-          id: n_approval.id
-        },
-        data: {
-          pdfPassword: password.toLocaleString(),
-          url: encryptedPdfPath,
-        }
-      })
     }
 
     return n_approval;
@@ -163,55 +149,5 @@ export class ApprovalLetterService {
     return await this.prisma.approvalLetter.delete({ where: { id } });
   }
 
-  private async pdfLink(dynamicData: any) {
 
-    const htmlPath = path.resolve('uploads', "template", "approval.html")
-
-    const pdfName = `${dynamicData.name}-${dynamicData.cfnId}.pdf` //TODO: change name with customer name
-
-    const outputPath = path.resolve('uploads', "original", pdfName)
-
-    const originalPdfPath = path.resolve('uploads', "original", pdfName)
-
-    const encryptedPdfPath = path.resolve('uploads', "protected", pdfName)
-
-    const password = Math.floor(100000 + Math.random() * 900000);
-
-    const populateTemplate = (template: any, data: any) => {
-      return template.replace(/{{(\w+)}}/g, (match: any, key: any) => {
-        return data[key] !== undefined ? data[key] : match;
-      });
-    };
-
-    const html = fs.readFileSync(htmlPath, "utf8");
-    const populatedHtml = populateTemplate(html, dynamicData);
-
-    (async () => {
-      const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox'] });
-
-      const page = await browser.newPage();
-      await page.setContent(populatedHtml);
-      await page.pdf({ path: outputPath });
-
-      await browser.close();
-    })();
-
-
-    // add two min pause here
-
-    setTimeout(() => {
-      execSync(`qpdf --encrypt ${password} ${password} 256 -- --allow-weak-crypto "${originalPdfPath}" "${encryptedPdfPath}"`);
-      fs.unlinkSync(originalPdfPath);
-    }
-      , 2000);
-
-
-
-    return {
-      originalPdfPath,
-      encryptedPdfPath,
-      password,
-      pdfName
-    }
-  }
 }
