25ec446aad51d7272c5558aeef1ba18b0794349d
Utpal Singh <utpalsingh915@gmail.com>
2024-05-18 11:28:33 +0530

    Refactor mail service switch case statements

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -4,46 +4,29 @@ import axios from 'axios';
 import mailEnums from 'src/utils/mailEnumbs';
 
 
-
 @Injectable()
 export class MailService {
   async sendSms(createMailDto: CreateMailDto) {
 
     let message: string;
-    const company = "Mahadev financial Services Pvt. Ltd.";
-    const company_full = "Mahadev financial services private limited Dalgreen";
+
     switch (createMailDto.type) {
-      // case mailEnums.WELCOME.toString():
-      //   message = `Congratulations ${createMailDto.value?.split("|")[0]} Your Documents has Been Approved By Company ${company} Download your Welcome latter.Your File No is ${createMailDto.value?.split("|")[1]} pass- ${createMailDto.value?.split("|")[2]} Thanks for Choose Us. ${company_full}& templateid=1607100000000293168`;
-      //   break;
-      case mailEnums.WELCOME.toString():
-        message = `Congratulations ${createMailDto.value?.split("|")[0]} Your Documents has Been Approved By Company Mahadev financial Services Pvt. Ltd. your Welcome latter has been generated .Your File No. is ${createMailDto.value?.split("|")[1]} Thanks for Choose Us. Mahadev financial Services Private Limited Dalgreen&templateid=1607100000000293168`;
-        break;
+
       case mailEnums.JOINING.toString():
-        message = `Congratulations! ${createMailDto.value?.split("|")[0]} Welcome to ${company} Your Joining has been Accepted By Company.Your Login ID And Password is ${createMailDto.value?.split("|")[1]}, pass - ${createMailDto.value?.split("|")[2]} Thank You. Dalgreen&templateid=1607100000000293167`;
+        message = `Congratulations! ${createMailDto.value?.split("|")[0]} Welcome to Fundwisor Finance Business Services Pvt. Ltd. Your Joining has been Accepted By Company. Your EMP ID ${createMailDto.value?.split("|")[1]}, pass-${createMailDto.value?.split("|")[2]} caslon &templateid=1707169985689943172`;
         break;
-      // case mailEnums.LOAN_APPLICATION.toString():
-      //   message = `Congratulations ${createMailDto.value?.split("|")[0]} Welcome to Caslon Business Services Pvt Ltd.We have received your Application.Your application No.is ${createMailDto.value?.split("|")[1]}. Team will Call You soon Thank you for Choose us.Caslon business Services Private Limited &templateid=1707169995602526224`;
-      //   break;b
-      case mailEnums.LOAN_APPROVED.toString():
-        message = `Congratulations! ${createMailDto.value?.split("|")[0]} Your Documents Has Been Approved and generated your Approval Latter Loan Amount ${createMailDto.value?.split("|")[1]} Rs Sanction by Company ${company} Thanks for Choose Us. ${company_full}& templateid=1607100000000293169`;
-        break;
-      case mailEnums.INVOICE.toString():
-        message = `Dear Customer ${createMailDto.value?.split("|")[0]} Your Processing Fee RS ${createMailDto.value?.split("|")[1]} has be received your Application in process thank you for Choose us.${company_full}& templateid=1607100000000293861`;
-        break;
-      case mailEnums.WELCOME_INVOICE.toString():
-        message = `Dear Customer ${createMailDto.value?.split("|")[0]} Your file charge Payment RS ${createMailDto.value?.split("|")[1]} has be received your Case File in process thank you for Choose us. ${company_full}& templateid=1607100000000293860`;
-        break;
+
     }
+
+
     try {
-      // await axios.get(`http://sms.smsindori.com/http-api.php?username=Mahadevt&password=123456&authentic-key=37364d616861646576743130301702819698&senderid=DALFOD&route=06&number=${createMailDto.numbers}&message=${message}`);
-      // console.log({
-      //   message,
-      //   value: createMailDto.value,
-      //   numbers: createMailDto.numbers,
-      //   date: new Date().toISOString()
-      // })
-      return true
+      await axios.get(`http://sms.smsindori.com/http-api.php?username=Caslon&password=123456&authentic-key=33304361736c6f6e3130301700970207&senderid=CASBUS&route=12&number=${createMailDto.numbers}&message=${message}`);
+      console.log({
+        message,
+        value: createMailDto.value,
+        numbers: createMailDto.numbers,
+        date: new Date().toISOString()
+      })
     } catch (error) {
       return true;
     }
