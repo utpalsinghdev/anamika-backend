b8dbcc5bcb0b5854d0ae65958843652528ffb444
utpal singh <utpalsingh915@gmail.com>
2025-10-27 12:37:10 +0530

    Refactor approval letter service and template to utilize environment variable for frontend domain, enhancing flexibility and maintainability of image sources in approval letters.

--- src/approval-letter/approval-letter.service.ts
+++ src/approval-letter/approval-letter.service.ts
@@ -312,7 +312,7 @@ export class ApprovalLetterService {
     const html = fs.readFileSync(htmlPath, "utf8");
     const populatedHtml = populateTemplate(html, {
       ...dynamicData,
-      FRONTEND_DOMAIN: "https://elfin.live"
+      FRONTEND_DOMAIN: process.env.FRONTEND_DOMAIN
     });
 
     (async () => {
